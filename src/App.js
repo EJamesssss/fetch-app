113899e388b9609043b3ff0199aa314a8b1c7b66
EJamesssss <ejames.hegina@gmail.com>
2022-04-04 21:36:36 +0800

    Added Update and Delete

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import Form from './Form'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import Form from "./Form";
+import "./App.css";
 
 function App() {
-  const [users, setUsers] = useState([])
-  const [hasError, setHasError] = useState(false)
-  const [errorMessage, setErrorMessage] = useState('')
-  const [isLoading, setIsLoading] = useState(true)
-  
+  const [users, setUsers] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [id, setId] = useState("");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
   const fetchUsers = () => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((result) => {
-        setUsers(result)
-        setIsLoading(false)
+        setUsers(result);
+        setIsLoading(false);
       })
       .catch((error) => {
-        setHasError(true)
-        setIsLoading(false)
-        setErrorMessage(error.message)
-      })
-  }
+        setHasError(true);
+        setIsLoading(false);
+        setErrorMessage(error.message);
+      });
+  };
 
   useEffect(() => {
-    fetchUsers()
-  }, [])
+    fetchUsers();
+  }, []);
+
+  // const handleDelete = (e) => {
+  //   e.preventDefault()
+  //   const endPoint = "https://jsonplaceholder.typicode.com/posts/" + id;
+
+  //   fetch(endPoint, {
+  //     method: "DELETE",
+  //   });
+  //   alert('Successfully Deleted')
+  // };
 
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Random Users</h1>
-      <Form userId={1} />
+      <Form userId={id} editTitle={title} editBody={body} />
       <br />
       <br />
       {hasError ? <p>{errorMessage}</p> : null}
@@ -41,6 +53,16 @@ function App() {
               <li key={id}>
                 <p>Title: {title}</p>
                 <p>Body: {body}</p>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault()
+                    setId(id);
+                    setTitle(title);
+                    setBody(body);
+                  }}
+                >
+                  Select
+                </button>
               </li>
               <hr />
             </>
@@ -50,7 +72,7 @@ function App() {
         <h3>loading...</h3>
       )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,55 +1,85 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const Form = ({ userId }) => {
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
+const Form = ({ userId, editTitle, editBody }) => {
+  const [title, setTitle] = useState(editTitle);
+  const [body, setBody] = useState(editBody);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const data = { userId, title, body }
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
+    const data = { userId, title, body };
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
       body: JSON.stringify(data),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     }).then((response) => {
-      console.log(response)
-      alert('Form Submitted')
-      return response.json()
-    })
-  }
+      console.log(response);
+      alert("Form Submitted");
+      return response.json();
+    });
+  };
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    const endPoint = "https://jsonplaceholder.typicode.com/posts/" + userId;
+    const data = { userId, title, body };
+
+    fetch(endPoint, {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((response) => {
+      console.log(response);
+      alert("Successfully updated!");
+      return response.json();
+    });
+  };
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    const endPoint = "https://jsonplaceholder.typicode.com/posts/" + userId;
+
+    fetch(endPoint, {
+      method: "DELETE",
+    });
+    alert("Successfully Deleted");
+  };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form>
         <h3>Create Post</h3>
         <div>userId {userId}</div>
         <div>
           <input
-            type='text'
-            name='title'
-            placeholder='Input Title'
+            type="text"
+            name="title"
+            placeholder="Input Title"
             onChange={(e) => setTitle(e.target.value)}
             value={title}
           />
         </div>
         <div>
           <input
-            type='text'
-            name='body'
+            type="text"
+            name="body"
             onChange={(e) => setBody(e.target.value)}
-            placeholder='Input Body'
+            placeholder="Input Body"
             value={body}
           />
         </div>
         <div>
-          <button type='submit'>Submit</button>
+          <button onClick={handleSubmit}>Submit</button>
+          <button onClick={handleEdit}>Edit</button>
+          <button onClick={handleDelete}>Delete</button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
